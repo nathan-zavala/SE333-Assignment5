6e91cb54464bdc1c407a409e697a3992fe6e62bd
nathan-zavala <nathan.zavala.0417@gmail.com>
2026-03-16 19:06:37 -0500

    Nathan added Playwright GitHub Actions

--- src/test/java/org/example/playwrightTraditional/BookstoreTest.java
+++ src/test/java/org/example/playwrightTraditional/BookstoreTest.java
@@ -17,7 +17,7 @@ public class BookstoreTest {
     @BeforeAll
     static void launchBrowser() {
         playwright = Playwright.create();
-        browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+        browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(true));
     }
 
     @AfterAll
